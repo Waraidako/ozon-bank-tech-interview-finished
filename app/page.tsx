1e65d7fca77fa4106082c591abfa8e3f98cd9aff
Waraidako <waraidako.god@gmail.com>
2026-09-18 16:17:48 +0300

    Inital commit - finished MVP

--- app/page.tsx
+++ app/page.tsx
@@ -1,69 +1,75 @@
-import Image from "next/image";
+"use client"
+
+import React, { useEffect, useState } from "react";
+
+const winningLines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4 ,6]
+]
+
+function checkEndgame(field: string[]): string { // returns "X"/"O" for a winner, "T" for a tie, "" for non-endgame
+    const winningLine = winningLines.find(([i, j, k]) => {
+        return field[i] && field[i] === field[j] && field[i] === field[k]
+    })
+    if (winningLine) return field[winningLine[0]];
+    if (field.every((cell) => cell)) return "T";
+    return "";
+}
 
 export default function Home() {
-  return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert h-5 w-[100px]"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the{" "}
-            <code className="rounded bg-black/[.06] px-1.5 py-0.5 font-mono text-[0.9em] dark:bg-white/[.08]">
-              page.tsx
-            </code>{" "}
-            file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
+
+  const [turnX, setTurnX] = useState(true);
+  const [field, setField] = useState(Array.from({ length: 9 }, () => ""));
+
+  const handleClick = (event: React.MouseEvent<HTMLDivElement>) => {
+    const target = event.target as HTMLElement | null;
+    if (!target) return;
+    const cell = target.closest(".cell") as HTMLElement | null;
+    if (!cell) return;
+    const cellIndex = cell.dataset.itemkey;
+    if (!cellIndex) return;
+    const fieldCopy = Array.from(field);
+    fieldCopy[Number(cellIndex)] = turnX ? "X" : "O";
+    setField(fieldCopy);
+    setTurnX(!turnX);
+    }
+
+    useEffect(() => {
+        const gameState = checkEndgame(field);
+        if (gameState) {
+            if (gameState === "T") alert("Tie");
+            else alert(`${gameState} won`);
+            setTimeout(() => {
+                setField(Array.from({ length: 9 }, () => ""));
+                setTurnX(true);
+            }, 0);
+        }
+    }, [field]);
+
+    return (
+        <div className={"wrapper flex flex-col w-full h-full justify-center items-center"}>
+            <div className={"playfield w-[300px] h-[300px] grid grid-cols-3 bg-gray-100"}
+                onClick={handleClick}
             >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+            {
+                field.map((el, index) => {
+                return <div
+                    key={index}
+                    data-itemkey={index}
+                    className={"cell w-[100px] h-[100px] border border-gray-500 flex justify-center items-center text-6xl font-bold"}
+                >
+                    <span className={"mb-2"}>{el}</span>
+                </div>
+                })
+            }
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert h-[14px] w-4"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={14}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
+      </div>
   );
+
 }
